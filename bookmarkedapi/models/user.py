8d3ee73066589907f5a8d6384c5f30932106cbe4
Whitley Beers <whitleybeers@gmail.com>
2023-08-19 10:56:56 -0500

    book model created

--- bookmarkedapi/models/user.py
+++ bookmarkedapi/models/user.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class User(models.Model):
     """User model"""
+    
     first_name = models.CharField(max_length=50)
     last_name =  models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
