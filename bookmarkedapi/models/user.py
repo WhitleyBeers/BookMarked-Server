cb0a583a231dc6aa63e982dcb0913d5443520ed6
Whitley Beers <whitleybeers@gmail.com>
2023-08-19 10:45:50 -0500

    user model created

---
+++ bookmarkedapi/models/user.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class User(models.Model):
+    """User model"""
+    first_name = models.CharField(max_length=50)
+    last_name =  models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    uid = models.CharField(max_length=50)
+    bio = models.CharField(max_length=254)
+    profile_image_url = models.URLField(max_length=254)
+    
+    @property
+    def following(self):
+        return self.__following
+    
+    @following.setter
+    def following(self, value):
+        self.__following = value
